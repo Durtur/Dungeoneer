37b8384b90401688eac64d0f70f2d7244394e6aa
Símon <simonorn@gmail.com>
2022-04-07 21:23:54 +0000

    #57

--- docs/client/map.client.js
+++ docs/client/map.client.js
@@ -26,16 +26,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     hammertime.on('pinchin', function (ev) {
         console.log(ev);
-        showBubblyText(ev.center.x)
-        ev.clientX = ev.center.x;
-        ev.clientY = ev.center.y;
+       
+        ev.x = ev.center.x;
+        ev.y = ev.center.y;
         zoomIntoMap(ev, -0.01)
     });
     hammertime.on('pinchout', function (ev) {
         console.log(ev);
-        showBubblyText(ev.center.x);
-        ev.clientX = ev.center.x;
-        ev.clientY = ev.center.y;
+     
+        ev.x = ev.center.x;
+        ev.y = ev.center.y;
         zoomIntoMap(ev, 0.01)
     });
     hammertime.get('pinch').set({ enable: true })
