4ce9bc206f2695626b2394a00badf5eee431abac
Durtur <simonorn@gmail.com>
2022-10-22 17:27:33 +0000

    config

--- docs/client/connectionConfig.js
+++ docs/client/connectionConfig.js
@@ -1,5 +1,5 @@
 var PEER_CONNECTION_CONFIG = {
-    secure: true,
-    host: "dungeoneer-peer-js-server.onrender.com",
-    path: "/myapp",
+    "secure": true,
+    "host": "dungeoneer-peer-js-server.onrender.com",
+    "path": "/myapp",
 };
